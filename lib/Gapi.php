9d1c78f693c26334020c3b6e196c135b78efbe3a
Hybrided <nbz_tricksta@hotmail.com>
2013-03-04 17:07:59 +0100

    removed useless default value

--- lib/Gapi.php
+++ lib/Gapi.php
@@ -119,7 +119,7 @@ class Gapi
    * @param Int $start_index OPTIONAL: Start index of results
    * @param Int $max_results OPTIONAL: Max results returned
    */
-  public function requestReportData($report_id, $dimensions=null, $metrics, $sort_metric=null, $filter=null, $start_date=null, $end_date=null, $start_index=1, $max_results=30)
+  public function requestReportData($report_id, $dimensions, $metrics, $sort_metric=null, $filter=null, $start_date=null, $end_date=null, $start_index=1, $max_results=30)
   {
     $parameters = array('ids'=>'ga:' . $report_id);
     
